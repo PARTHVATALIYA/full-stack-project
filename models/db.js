17b20677f22fcbe0b4a65d50341fa1c423bc8445
parth <parth>
2023-06-09 12:31:45 +0530

    update db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // Allow Promises
 mongoose.Promise = global.Promise;
 // Connection
-mongoose.connect('mongodb://3.6.91.164:27017/db_test', { useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:true  });
+mongoose.connect('mongodb://13.233.97.75:27017/db_test', { useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:true  });
 // Validation
 mongoose.connection
   .once('open', () => console.log('Connected to the database!'))
